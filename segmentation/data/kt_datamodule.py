34a9d802f342d58d7c0be9a17c35b6d6bae3974a
bigden <mangazeev.di@phystech.edu>
2024-02-09 12:43:04 +0300

    add: docstring for KTDataset and KTDataModule

--- segmentation/data/kt_datamodule.py
+++ segmentation/data/kt_datamodule.py
@@ -11,12 +11,34 @@ from segmentation.data.transforms import ClipAndNorm, ConvertMask
 
 
 class KTDataset(Dataset):
+    """
+    Pytorch dataset for the VisionLabTask.
+
+    Attributes:
+        data_dir (Path): Root dataset directory
+        files (list[str]): List of file names without extension
+        transforms (A.Compose | None): Albumentations transformation, Defaults to `None`
+
+    Methods:
+        __init__: Initialize the `KTDataset` object
+        __len__: Return number of files
+        __getitem__: Return img and mask by index
+
+    """
     def __init__(
             self,
             data_dir: Path,
             files: list[str],
             transforms: A.Compose | None = None,
     ) -> None:
+        """
+        Initialize the `KTDataset` object.
+
+        Args:
+            data_dir (Path): Root dataset directory
+            files (list[str]): List of file names without extension
+            transforms (A.Compose | None): Albumentations transformation, Defaults to `None`
+        """
         super().__init__()
 
         self.data_dir: Path = data_dir
@@ -25,9 +47,23 @@ class KTDataset(Dataset):
         self.transforms: A.Compose | None = transforms
 
     def __len__(self):
+        """
+        Returns:
+            number of files
+        """
         return len(self.files)
 
     def __getitem__(self, idx: int):
+        """
+        Takes an image and a mask by index, applies augmentations to them.
+
+        Args:
+            idx (int): Index of the file
+
+        Returns:
+            img (np.ndarray | torch.Tensor): augmented image.
+            mask (np.ndarray | torch.Tensor): augmented mask.
+        """
         img = np.load(str(self.data_dir / 'images' / f"{self.files[idx]}_img.npy"))
         mask = np.load(str(self.data_dir / 'labels' / f"{self.files[idx]}_lbl.npy"))
 
@@ -39,7 +75,25 @@ class KTDataset(Dataset):
 
 
 class KTDataModule(LightningDataModule):
+    """
+    `LightningDataModule` for the VisionLabTask dataset.
 
+    The proposed data is obtained from the open datasets TotalSegmentator and AMOS22
+    by sampling several coronal views from the 3D study array.
+
+    Attributes:
+        data_train (Dataset | None): Training dataset
+        data_val (Dataset | None): Validation dataset
+        data_test (Dataset | None): Test dataset
+
+    Methods:
+        __init__: Initialize the `KTDataModule` object
+        setup: load data, Split to train val test
+        train_dataloader: Create and return the train dataloader
+        val_dataloader: Create and return the validation dataloader
+        test_dataloader: Create and return the test dataloader
+
+    """
     def __init__(
             self,
             data_dir: str,
@@ -52,6 +106,21 @@ class KTDataModule(LightningDataModule):
             pin_memory: bool = False,
             seed: int = 42,
     ) -> None:
+        """
+        Initialize a `KTDataModule`.
+
+        Args:
+            data_dir (str): Path to dataset directory
+            train_transforms (A.Compose | None): Albumentations train transformation, Defaults to `None`
+            val_transforms (A.Compose | None): Albumentations validation transformation, Defaults to `None`
+            test_transforms (A.Compose | None): Albumentations test transformation, Defaults to `None`
+            train_val_test_split (tuple[float, float, float]): The train, validation and test split.
+                Defaults to `(0.8, 0.1, 0.1)`.
+            batch_size (int): The batch size. Defaults to `64`.
+            num_workers (int): The number of workers. Defaults to `0`.
+            pin_memory (bool): Whether to pin memory. Defaults to `False`.
+            seed (int): Random seed. Defaults to `42`.
+        """
         super().__init__()
 
         # this line allows to access init params with 'self.hparams' attribute
@@ -63,7 +132,18 @@ class KTDataModule(LightningDataModule):
         self.data_test: Dataset | None = None
 
     def setup(self, stage: str | None = None) -> None:
+        """
+        Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
 
+        This method is called by Lightning before `trainer.fit()`, `trainer.validate()`, `trainer.test()`, and
+        `trainer.predict()`, so be careful not to execute things like random split twice! Also, it is called after
+        `self.prepare_data()` and there is a barrier in between which ensures that all the processes proceed to
+        `self.setup()` once the data is prepared and available for use.
+
+        Args:
+            stage (str | None): The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
+                Defaults to ``None``.
+        """
         # load and split datasets only if not loaded already
         if not self.data_train and not self.data_val and not self.data_test:
 
@@ -101,6 +181,12 @@ class KTDataModule(LightningDataModule):
             )
 
     def train_dataloader(self) -> DataLoader:
+        """
+        Create and return the train dataloader.
+
+        Returns:
+            train_dataloader (DataLoader)
+        """
         return DataLoader(
             dataset=self.data_train,
             batch_size=self.hparams.batch_size,
@@ -110,6 +196,12 @@ class KTDataModule(LightningDataModule):
         )
 
     def val_dataloader(self) -> DataLoader:
+        """
+        Create and return the validation dataloader.
+
+        Returns:
+            val_dataloader (DataLoader)
+        """
         return DataLoader(
             dataset=self.data_val,
             batch_size=1,
@@ -119,6 +211,12 @@ class KTDataModule(LightningDataModule):
         )
 
     def test_dataloader(self) -> DataLoader:
+        """
+        Create and return the test dataloader.
+
+        Returns:
+            test_dataloader (DataLoader)
+        """
         return DataLoader(
             dataset=self.data_test,
             batch_size=1,
